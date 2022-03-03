7c64ac415a1391b00b1a5ce570c8078246fc2ed0
Abdulrahman Saber <73113911+asaber3030@users.noreply.github.com>
2022-03-03 15:09:43 +0200

    Updating comments

--- Task2-Gradient-Descent-Linear-Regression/main.py
+++ Task2-Gradient-Descent-Linear-Regression/main.py
@@ -3,21 +3,21 @@ import numpy as np
 import csv
 
 def gradient_descent(x, y):
-  m_curr = b_curr = 0  # starting values
+  m_curr = b_curr = 0  # starting values [ initial values of thetas ], Theta-1 = 0, Theta-0 = 0
   n = len(x)  # no. of datapoints
   learning_rate = 0.08
 
   for i in range(n):
-    y_pred = m_curr * x + b_curr
+    y_pred = m_curr * x + b_curr # hypothesis function - predicted value
     cost = (1 / n) * sum([
       val ** 2 for val in (y - y_pred)
-    ])
+    ]) # cost function
 
-    md = - (2 / n) * sum(x * (y - y_pred))
-    bd = - (2 / n) * sum(y - y_pred)
+    bd = - (2 / n) * sum(y - y_pred) # theta0
+    md = - (2 / n) * sum(x * (y - y_pred)) # theta1
     
-    m_curr = m_curr - learning_rate * md
     b_curr = b_curr - learning_rate * bd
+    m_curr = m_curr - learning_rate * md
 
   print(md, bd)
 
